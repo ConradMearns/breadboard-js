8db0b7ad57d7477b058b9a0ecdad3c79b4b67209
Conrad Mearns <conradmearns@pm.me>
2021-01-18 20:50:23 -0800

    WIP

--- src/server.js
+++ src/server.js
@@ -6,10 +6,11 @@ import * as sapper from '@sapper/server';
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
 
-const url = dev ? '/' : 'breadboard-js';
+// const url = dev ? '/' : 'breadboard-js';
 
 polka() // You can also use Express
 	.use(
+		// url,
 		compression({ threshold: 0 }),
 		sirv('static', { dev }),
 		sapper.middleware()
